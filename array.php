a77a66cd0714b0b7f90d0828f33c8078afae4c4d
Rohan Goyal <43959055+goelrohan94@users.noreply.github.com>
2020-10-01 12:54:50 +0530

    More explainable Variable name

--- array.php
+++ array.php
@@ -2,22 +2,22 @@
 $file = __FILE__;
 include 'highlight.php';
 
-$a1 = array('elem1'=>array('name'=> 'John Doe', 'age'=>32), 'elem2'=>array('name'=> 'Leonidas Lampion', 'age'=>53));
-$a2  = array('elem1' => array(
+$a1 = array('element1'=>array('name'=> 'John Doe', 'age'=>32), 'element2'=>array('name'=> 'Leonidas Lampion', 'age'=>53));
+$a2  = array('element1' => array(
 						'name'=> 'John Doe',
 						'age'=>32
 						),
-			'elem2' => array(
+			'element2' => array(
 						'name'=> 'Leonidas Lampion',
 						'age'=>53
 						)
 			);
 			
-$a3  = ['elem1' => [
+$a3  = ['element1' => [
 						'name'=> 'John Doe',
 						'age'=>32
 						],
-			'elem2' => [
+			'element2' => [
 						'name'=> 'Leonidas Lampion',
 						'age'=>53
 						]
